a9cc561a3826f54a632e27be21d56ba841849ab0
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-09-23 09:13:39 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import { Container, Header, Link } from './App.styled';
+import svg from '../images/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        {/* <a href="/" aria-label="logo">
+          <svg width="100" height="40">
+            <use>{svg}</use>
+          </svg>
+        </a> */}
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>}></Route>
+      </Routes>
+    </Container>
   );
 };
